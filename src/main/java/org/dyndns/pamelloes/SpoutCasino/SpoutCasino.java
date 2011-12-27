b5800e6c1e68f4da43c904b6e37c1b4f88d832dd
Pamelloes <joshuabrot@gmail.com>
2011-12-26 20:19:37 -0600

    Set up project and copied in images.

---
+++ src/main/java/org/dyndns/pamelloes/SpoutCasino/SpoutCasino.java
@@ -0,0 +1,16 @@
+package org.dyndns.pamelloes.SpoutCasino;
+
+import org.getspout.spoutapi.plugin.SpoutPlugin;
+
+public class SpoutCasino extends SpoutPlugin {
+
+	public void onEnable() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void onDisable() {
+		// TODO Auto-generated method stub
+
+	}
+}
